d312678fd127c5c3512dae0ed04d50e21746b916
Cheyenne Kellis <cheyenne.kellis@gmail.com>
2015-10-07 12:25:07 -0700

    Pass specs for stacks

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,34 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var length = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[length] = value;
+    length++;
   };
 
   someInstance.pop = function() {
+    var result;
+    if(length > 0){
+      length--;
+      result = storage[length];
+      delete storage[length];
+    }
+    return result;
   };
 
   someInstance.size = function() {
+    return length;
   };
-
   return someInstance;
 };
+var stack = new Stack();
+
+stack.push('a');
+        stack.push('b');
+        console.log(stack.pop());
+
+
+
