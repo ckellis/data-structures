f39a9274ef27ffbca17a67c3fb2d924bd6332e01
Cheyenne Kellis <cheyenne.kellis@gmail.com>
2015-10-07 13:30:27 -0700

    Passes specs until queue-specific behavior

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,27 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var length = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[length] = value;
+    length++;
   };
 
   someInstance.dequeue = function() {
+    var result;
+    if(length > 0){
+      length--;
+      result = storage[0];
+      delete storage[0];
+    }
+    return result;
   };
 
   someInstance.size = function() {
+    return length;
   };
 
   return someInstance;
