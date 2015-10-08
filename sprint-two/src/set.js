990ed7bf11ebfe210651886e6b23e6c262187278
ckellis <cheyenne.kellis@gmail.com>
2015-10-08 15:07:46 -0700

    Pass specs for sets

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,19 +1,28 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
-  set._storage = undefined;
+  if(!this.contains(item)){
+    this._storage.push(item);  
+  }
 };
 
 setPrototype.contains = function(item) {
+  if(this._storage.indexOf(item) > -1){
+    return true;
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  if(this.contains(item)){
+    this._storage.splice(this._storage.indexOf(item), 1);
+  }
 };
 
 /*
