ad9b0bd3fab701700324cf84dace042a4932e74a
Hack Reactor Students <accounts@hackreactor.com>
2015-10-08 10:27:22 -0700

    Pass specs for linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,34 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if(!list.head){
+      list.head = newNode;
+    } else {
+      list.tail.next = newNode;
+    }
+    list.tail = newNode;
+
   };
 
   list.removeHead = function() {
+    if(list.head){
+      var removed = list.head.value;
+      list.head = list.head.next;
+      return removed;
+    }
   };
 
   list.contains = function(target) {
+    var currentNode = list.head;
+    while(currentNode){
+      if(currentNode.value === target){
+        return true;
+      } else {
+        currentNode = currentNode.next;
+      }
+    }
+    return false;
   };
 
   return list;
