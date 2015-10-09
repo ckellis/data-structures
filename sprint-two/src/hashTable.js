25d39408b5b51f77810dd7305fb2cc00f6f75def
ckellis <cheyenne.kellis@gmail.com>
2015-10-08 21:08:18 -0700

    Pass specs for hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,18 +6,24 @@ var HashTable = function() {
 };
 
 HashTable.prototype.insert = function(k, v) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, v);
+   var index = getIndexBelowMaxForKey(k, this._limit);
+  if(this._storage.get(index)){
+    this._storage.get(index)[k] = v;
+  } else {
+    var bucket = {};
+    bucket[k] = v;
+    this._storage.set(index, bucket);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index);
+  return this._storage.get(index)[k];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, null);
+  this._storage.get(index)[k] = null;
 };
 
 
