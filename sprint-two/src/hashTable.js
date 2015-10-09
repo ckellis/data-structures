761ee92e15d45c62f94af96c1ba3f79f6bd85440
ckellis <cheyenne.kellis@gmail.com>
2015-10-09 15:49:04 -0700

    Pass extra resizing specs for hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,28 +3,39 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._size = 0;
 };
 
 HashTable.prototype.insert = function(k, v) {
-   var index = getIndexBelowMaxForKey(k, this._limit);
-   var bucket = this._storage.get(index);
+
+  if(this._size >= (this._limit * .75)){
+    this.resize(this._limit * 2);
+  }
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
   if(bucket !== undefined){
     for(var i = 0; i < bucket.length; i++){
-      if(bucket[i][0] === k)
+      if(bucket[i][0] === k){
         bucket[i][1] = v;
-      else
+      } else{
         bucket.push([k,v]);
+        this._size++;
       }
+    }
   } else {
     bucket = [];
     bucket.push([k,v]);
     this._storage.set(index, bucket);
+    this._size++;
   }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(index);
+  if(bucket === undefined){
+    return null;
+  }
   for(var i = 0; i < bucket.length; i++){
     if(bucket[i][0] === k){
       return bucket[i][1];
@@ -38,8 +49,29 @@ HashTable.prototype.remove = function(k) {
   for(var i = 0; i < bucket.length; i++){
     if(bucket[i][0] === k){
       bucket[i][1] = null;
+      bucket.splice(i,1);
+      this._size--;
     }
   }
+  if(this._size < (this._limit * .25)){
+    this.resize(this._limit / 2);
+  }
+};
+
+HashTable.prototype.resize = function(newLimit){
+  var oldStore = this._storage;
+  
+  this._limit = newLimit;
+  this._storage = LimitedArray(newLimit);
+  this._size = 0;
+
+  oldStore.each(function(bucket){
+    if(bucket){  
+      for(var i = 0; i < bucket.length; i++){
+        this.insert(bucket[i][0],bucket[i][1]);
+      }
+    }
+  }.bind(this));
 };
 
 
